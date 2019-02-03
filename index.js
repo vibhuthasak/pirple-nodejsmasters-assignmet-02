a1f9fdeb633d9ea0109736188e1a6cb2c2623079
Vibhutha Kumarage <vkartz@gmail.com>
2019-02-03 16:55:45 +0530

    Remove unwanted files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Dependencides
 var server = require('./lib/server');
-var workers = require('./lib/workers');
 
 // Declare the app
 var app = {}
@@ -10,8 +9,6 @@ app.init = function(){
   // Start the server
   server.init()
 
-  // Start the worker
-  workers.init()
 }
 
 // Execute
