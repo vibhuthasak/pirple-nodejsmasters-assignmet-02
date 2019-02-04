fde6c5a19681f7916e1ca2a0154e007df2eed8b2
Vibhutha Kumarage <vkartz@gmail.com>
2019-02-04 12:22:07 +0530

    Mailgun handler added

--- config.js
+++ config.js
@@ -7,7 +7,11 @@ environments.staging = {
   'port': 3000,
   'envName': 'staging',
   'hashingSecret' : 'thisIsMySecret',
-  'stripeSkey': 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+  'stripeSkey': 'sk_test_4eC39HqLyjWDarjtT1zdp7dc',
+  'mailgun': {
+    'username': 'api',
+    'password': '675defeb9765847066f37fadeccba5d5-b9c15f4c-c7310b91'
+  }
 };
 
 environments.production = {
